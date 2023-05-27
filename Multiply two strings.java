29775d4d444d1b2ceec21f3a7003d2e256bab228
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2023-05-27 10:07:21 +0530

    Update Multiply two strings.java

--- Multiply two strings.java
+++ Multiply two strings.java
@@ -1,1 +1,18 @@
 
+
+//User function Template for Java
+
+class Solution
+{
+    public String multiplyStrings(String s1,String s2)
+    {
+        
+        BigInteger a = new BigInteger(s1);
+        BigInteger b = new BigInteger(s2);
+        BigInteger result;
+        result=a.multiply(b);
+        String r  = result.toString();
+        return r;
+        //code here.
+    }
+}
